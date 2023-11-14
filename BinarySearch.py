b2e8419da4c88aeb917ecb01fd0a0a3005b39841
Callum Boylan <2boylc17@solent.ac.uk>
2023-11-14 16:09:07 +0000

    yes3

--- BinarySearch.py
+++ BinarySearch.py
@@ -3,15 +3,20 @@ import math
 arr = [173, 179, 271, 364, 382, 390, 453, 509, 515, 566, 649, 737, 758, 836, 929]
 newarr = arr
 found = False
-searchValue = 737
+print(arr)
+searchValue = int(input("What number are you searching for?\n"))
 start = 0
 end = len(arr) - 1
 while found is False:
     middle = math.floor((start + end) / 2)
+    print(start, end, middle)
     if newarr[middle] == searchValue:
         print(f"Found value {searchValue} at index {middle}")
         found = True
+    elif start == end:
+        print(f"Value '{searchValue}' is not in the list")
+        found = True
     elif newarr[middle] > searchValue:
-        end = middle
+        end = middle - 1
     elif newarr[middle] < searchValue:
-        start = middle
+        start = middle + 1
